16058ec12bbd955a9a725a5308a66c8003bd3693
aminulcloudpractice <aminulcloudpractice@gmail.com>
2026-01-06 11:38:38 -0600

    Again modify vm_size to main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     name            = "agentpool"
     node_count      = var.node_count
     # vm_size         = "standard_b2ms"
-    vm_size         = "standard_d2as_v6"
+    vm_size         = "Standard_B2s_v2"
   }
 
   service_principal {
